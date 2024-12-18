f3f957824b6f399a8bc2d721915a28a92af568b3
Russell Li <rli42@holylogin05.rc.fas.harvard.edu>
2024-12-18 00:39:12 -0500

    fixed dataset gen for distilled

--- running-russell.sh
+++ running-russell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=learner-train-pythia
+#SBATCH --job-name=learner-distill-pythia-datagen
 #SBATCH --account=kempner_emalach_lab
 #SBATCH --output=/n/holylfs06/LABS/kempner_fellow_emalach/Lab/rli/2281-Project/logs/%j/logs.out
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -30,6 +30,75 @@ cd /n/holylfs06/LABS/kempner_fellow_emalach/Lab/rli/2281-Project
 #export SPECIAL_TOKEN="<|sep|>"
 
 #run the script
+
+TARGET_MODEL="EleutherAI/pythia-2.8b"
+METRIC="lk"
+BATCH_SIZE=4
+EPOCHS=1
+DATASET_NAME="wikitext"
+DATASET_CONFIG="wikitext-2-raw-v1"
+DATASET_SPLIT="train"
+MAX_EXAMPLES=50000
+SIZES="70 160 410 70 160 410 70 160 410 70 160 410"
+
+DRAFTERS_TEMP1=(
+    distillation_lambada/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_cimec/lambada_temperature_1.0_2024-12-17_19-02-08
+    distillation_lambada/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_cimec/lambada_temperature_1.0_2024-12-17_18-42-11
+    distillation_lambada/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_cimec/lambada_temperature_1.0_2024-12-17_18-23-04
+    distillation_openwebtext_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_stas/openwebtext-10k_temperature_1.0_2024-12-17_19-01-51
+    distillation_openwebtext_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_stas/openwebtext-10k_temperature_1.0_2024-12-17_18-39-17
+    distillation_openwebtext_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_stas/openwebtext-10k_temperature_1.0_2024-12-17_18-23-03
+    distillation_pile_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_NeelNanda/pile-10k_temperature_1.0_2024-12-17_19-03-16
+    distillation_pile_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_NeelNanda/pile-10k_temperature_1.0_2024-12-17_18-48-44
+    distillation_pile_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_NeelNanda/pile-10k_temperature_1.0_2024-12-17_18-23-10
+    distillation_redpajama_v2/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_vilm/RedPajama-v2-small_temperature_1.0_2024-12-17_19-07-08
+    distillation_redpajama_v2/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_vilm/RedPajama-v2-small_temperature_1.0_2024-12-17_18-50-18
+    distillation_redpajama_v2/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_vilm/RedPajama-v2-small_temperature_1.0_2024-12-17_18-23-11
+)
+
+DRAFTERS_TEMP2=(
+    distillation_lambada/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_cimec/lambada_temperature_2.0_2024-12-17_18-20-56
+    distillation_lambada/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_cimec/lambada_temperature_2.0_2024-12-17_18-21-06
+    distillation_lambada/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_cimec/lambada_temperature_2.0_2024-12-17_18-21-33
+    distillation_openwebtext_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_stas/openwebtext-10k_temperature_2.0_2024-12-17_18-20-46
+    distillation_openwebtext_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_stas/openwebtext-10k_temperature_2.0_2024-12-17_18-21-06
+    distillation_openwebtext_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_stas/openwebtext-10k_temperature_2.0_2024-12-17_18-21-33
+    distillation_pile_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_NeelNanda/pile-10k_temperature_2.0_2024-12-17_19-20-48
+    distillation_pile_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_NeelNanda/pile-10k_temperature_2.0_2024-12-17_18-21-06
+    distillation_pile_10k/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_NeelNanda/pile-10k_temperature_2.0_2024-12-17_18-21-33
+    distillation_redpajama_v2/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-70m_vilm/RedPajama-v2-small_temperature_2.0_2024-12-17_18-21-35
+    distillation_redpajama_v2/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-160m_vilm/RedPajama-v2-small_temperature_2.0_2024-12-17_18-21-35
+    distillation_redpajama_v2/EleutherAI_pythia-2.8b_to_EleutherAI_pythia-410m_vilm/RedPajama-v2-small_temperature_2.0_2024-12-17_18-21-35
+)
+#temperature 1
+# python main.py \
+#     --mode create_dataset \
+#     --target_model_name "$TARGET_MODEL" \
+#     --drafters "${DRAFTERS_TEMP1[@]}" \
+#     --sizes $SIZES \
+#     --metric $METRIC \
+#     --ptfile pythia_distilled_allDrafters_lk-epochs${EPOCHS}-batch${BATCH_SIZE}_temp1.0.pt \
+#     --batch_size $BATCH_SIZE \
+#     --epochs $EPOCHS \
+#     --dataset_name $DATASET_NAME \
+#     --dataset_config $DATASET_CONFIG \
+#     --dataset_split $DATASET_SPLIT \
+#     --max_examples $MAX_EXAMPLES
+
+#temperature 2
+python main.py \
+    --mode create_dataset \
+    --target_model_name "$TARGET_MODEL" \
+    --drafters "${DRAFTERS_TEMP2[@]}" \
+    --sizes $SIZES \
+    --metric $METRIC \
+    --ptfile pythia_distilled_allDrafters_lk-epochs${EPOCHS}-batch${BATCH_SIZE}_temp2.0.pt \
+    --batch_size $BATCH_SIZE \
+    --epochs $EPOCHS \
+    --dataset_name $DATASET_NAME \
+    --dataset_config $DATASET_CONFIG \
+    --dataset_split $DATASET_SPLIT \
+    --max_examples $MAX_EXAMPLES
 
 # python main.py --mode train_learner \
 #     --target_model_name EleutherAI/pythia-2.8b \
